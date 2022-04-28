31d4b4641956e569a371cf6f3254fd1872dd1fe1
Besar Rahmat <besarrahmat007@gmail.com>
2022-04-29 03:30:25 +0700

    Initial commit

---
+++ migrations/1651166186383_create-table-albums.js
@@ -0,0 +1,30 @@
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('albums', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        name: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+        },
+        year: {
+            type: 'INTEGER',
+            notNull: true,
+        },
+        created_at: {
+            type: 'TIMESTAMP',
+            notNull: true,
+        },
+        updated_at: {
+            type: 'TIMESTAMP',
+            notNull: true,
+        },
+    });
+};
+
+exports.down = pgm => {
+    pgm.dropTable('albums');
+};
---
+++ migrations/1651171530239_create-table-songs.js
@@ -0,0 +1,53 @@
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('songs', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        title: {
+            type: 'VARCHAR(100)',
+            notNull: true,
+        },
+        year: {
+            type: 'INTEGER',
+            notNull: true,
+        },
+        genre: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+        },
+        performer: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+        },
+
+        duration: {
+            type: 'INTEGER',
+            notNull: false,
+        },
+        album_id: {
+            type: 'VARCHAR(50)',
+            notNull: false,
+        },
+        created_at: {
+            type: 'TIMESTAMP',
+            notNull: true,
+        },
+        updated_at: {
+            type: 'TIMESTAMP',
+            notNull: true,
+        },
+    });
+
+    pgm.addConstraint(
+        'songs',
+        'fk_albums_album_id',
+        'FOREIGN KEY (album_id) REFERENCES albums(id) ON DELETE CASCADE',
+    );
+};
+
+exports.down = pgm => {
+    pgm.dropTable('songs');
+};
